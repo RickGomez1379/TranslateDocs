21b666c1a095105a46edc16160809756cba7082e
Rick <rgomez2@student.fullsail.edu>
2024-04-18 16:45:47 -0400

    Added Home Screen with Buttons (Choose Picture From Gallery, Take Photo, Translator, Settings)

---
+++ app/src/main/java/com/example/translatedocs/Home.java
@@ -0,0 +1,14 @@
+package com.example.translatedocs;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+public class Home extends Activity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_activity);
+    }
+}
