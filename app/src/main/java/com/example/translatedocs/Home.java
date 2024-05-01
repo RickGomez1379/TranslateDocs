910bd2f934e87c6c808398a7c1efcfdf9d801e6c
Rick <rgomez2@student.fullsail.edu>
2024-05-01 17:48:09 -0400

    Added Translator Activity and Layout w/ Necessary Items (Spinner, Textinput, TextView) Added Translator Button Functionality in Home Activity Added String Array to strings.xml for Spinner Items

--- app/src/main/java/com/example/translatedocs/Home.java
+++ app/src/main/java/com/example/translatedocs/Home.java
@@ -5,24 +5,25 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
-import android.view.View;
 import androidx.annotation.Nullable;
 import android.widget.Button;
 
 public class Home extends Activity{
     Button galleryButton;
+    Button translatorButton;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_activity);
         galleryButton = findViewById(R.id.photoFromGalleryBtn);
-
-        galleryButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-                startActivityForResult(intent, 1);
-            }
+        translatorButton = findViewById(R.id.translatorBtn);
+        galleryButton.setOnClickListener(v -> {
+            Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            startActivityForResult(intent, 1);
+        });
+        translatorButton.setOnClickListener(v -> {
+            Intent intent = new Intent(this, TranslatorActivity.class);
+            startActivity(intent);
         });
     }
 
