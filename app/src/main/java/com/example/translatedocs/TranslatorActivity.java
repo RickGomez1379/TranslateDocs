2703b42636c0fe18c1673277dc0e82c9f2a56d2a
Rick <rgomez2@student.fullsail.edu>
2024-05-25 15:16:22 -0400

    Added Microphone Feature in TranslatorActivity Fixed TranslatorActivity Translation (Was Not Translating Correctly) Using SelectedItemPosition Instead of SelectedItem

--- app/src/main/java/com/example/translatedocs/TranslatorActivity.java
+++ app/src/main/java/com/example/translatedocs/TranslatorActivity.java
@@ -1,10 +1,14 @@
 package com.example.translatedocs;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
+
 import androidx.appcompat.widget.Toolbar;
 
 import androidx.annotation.Nullable;
@@ -19,10 +23,12 @@ import com.google.mlkit.nl.translate.Translation;
 import com.google.mlkit.nl.translate.Translator;
 import com.google.mlkit.nl.translate.TranslatorOptions;
 
+import java.util.Locale;
 import java.util.Objects;
 
 public class TranslatorActivity extends AppCompatActivity {
     Button translateButton;
+    ImageView mic;
     Spinner languagesFrom;
     Spinner languagesTo;
     String translateFrom;
@@ -30,12 +36,14 @@ public class TranslatorActivity extends AppCompatActivity {
     TextView translationTextView;
     TextInputEditText textToTranslateFrom;
     Toolbar nav;
+    int SPEECH_CODE = 102;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.translator_activity);
         translateButton = findViewById(R.id.translateBtn);
+        mic = findViewById(R.id.microphoneImageView);
         languagesFrom = findViewById(R.id.languagesFromSpinner);
         languagesTo = findViewById(R.id.languagesToSpinner);
         translationTextView = findViewById(R.id.translationTA);
@@ -45,36 +53,62 @@ public class TranslatorActivity extends AppCompatActivity {
         setSupportActionBar(nav);
         Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
 
+        mic.setOnClickListener(v -> OpenMicrophone());
+
 
         translateButton.setOnClickListener(v -> {
             //Set Language Codes
-            translateFrom = ChosenLanguage(languagesFrom.getSelectedItem().toString());
-            translateTo = ChosenLanguage(languagesTo.getSelectedItem().toString());
+            translateFrom = ChosenLanguage(languagesFrom.getSelectedItemPosition());
+            translateTo = ChosenLanguage(languagesTo.getSelectedItemPosition());
 
             //Translate
             Translate(Objects.requireNonNull(textToTranslateFrom.getText()).toString(), translateFrom,translateTo);
         });
     }
 
-    private String ChosenLanguage(String language) {
+    private void OpenMicrophone(){
+//        if(!SpeechRecognizer.isRecognitionAvailable(TranslatorActivity.this)){
+//            Toast.makeText(this, "Speech recognition is not available", Toast.LENGTH_SHORT).show();
+//        }
+//        else{}
+            Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+            intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+            intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+            intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Say Something!");
+            startActivityForResult(intent, SPEECH_CODE);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == SPEECH_CODE && resultCode == RESULT_OK){
+
+            String [] result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS).toArray(new String[0]);
+            textToTranslateFrom.setText(result[0]);
+        }
+    }
+
+    //TODO
+    private String ChosenLanguage(int language) {
         switch (language){
 
-            case "Spanish":
+            case 1:
                 return "es";
 
-            case "French":
+            case 2:
                 return "fr";
 
-            case "Germany":
+            case 3:
                 return "de";
 
-            case "Portuguese":
+            case 4:
                 return "pt";
 
-            case "Italian":
+            case 5:
                 return "it";
 
-            case "Romanian":
+            case 6:
                 return "ro";
 
             default:
